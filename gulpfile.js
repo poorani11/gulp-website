fe96ff337dc11daf3da66a6f88a1d289adc1b329
poorani11 <poorani.mgr@gmail.com>
2018-06-03 20:47:11 +0530

    Added the gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp =  require('gulp');
+
+//Styles
+gulp.task('styles', function() {
+    console.log('starting styles task');
+});
+
+//scripts
+gulp.task('scripts', function(){
+    console.log('Starting scripts task');  
+})
+
+//images
+gulp.task('images', function(){
+    console.log('Starting images tasks');
+})
+
+gulp.task('default', function(){
+    console.log('starting default task');
+});
+
+
+

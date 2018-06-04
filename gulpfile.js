84af7c474e6643918c20d9d8c9396a5c896af6c9
poorani11 <poorani.mgr@gmail.com>
2018-06-04 14:11:55 +0530

    adeed livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp =  require('gulp');
 var uglify = require('gulp-uglify');
+var livereload = require('gulp-livereload');
 
 //file paths
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
@@ -15,7 +16,8 @@ gulp.task('scripts', function(){
     
     return gulp.src(SCRIPTS_PATH)
          .pipe(uglify())
-                .pipe(gulp.dest('public/dist'));
+         .pipe(gulp.dest('public/dist'))
+         .pipe(livereload());
 })
 
 //images
@@ -30,6 +32,7 @@ gulp.task('default', function(){
 gulp.task('watch', function(){
     console.log('starting watch task');
     require('./server.js');
+    livereload.listen();
     gulp.watch(SCRIPTS_PATH, ['scripts']);
 });
 

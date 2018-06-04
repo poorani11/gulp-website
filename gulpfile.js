1cc2b3b23d58845d4e656265202f45d293219b03
poorani11 <poorani.mgr@gmail.com>
2018-06-04 14:37:06 +0530

    added gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp =  require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
 
 //file paths
+var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var CSS_PATH = 'public/css/**/*.css';
 
 //Styles
 gulp.task('styles', function() {
     console.log('starting styles task');
+    return gulp.src(['public/css/reset.css', CSS_PATH])
+            .pipe(concat('styles.css'))
+            .pipe(gulp.dest(DIST_PATH))
+            .pipe(livereload());
 });
 
 //scripts
@@ -16,7 +23,7 @@ gulp.task('scripts', function(){
     
     return gulp.src(SCRIPTS_PATH)
          .pipe(uglify())
-         .pipe(gulp.dest('public/dist'))
+         .pipe(gulp.dest(DIST_PATH))
          .pipe(livereload());
 })
 

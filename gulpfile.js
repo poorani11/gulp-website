d907ddbb17b393ab0ca97b84845cdd0ef7f83475
poorani11 <poorani.mgr@gmail.com>
2018-06-04 14:59:21 +0530

    Added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 //file paths
 var DIST_PATH = 'public/dist';
@@ -13,6 +14,9 @@ var CSS_PATH = 'public/css/**/*.css';
 gulp.task('styles', function() {
     console.log('starting styles task');
     return gulp.src(['public/css/reset.css', CSS_PATH])
+            .pipe(autoprefixer({
+               browsers: ['last 2 versions', 'ie 8']
+             }))
             .pipe(concat('styles.css'))
             .pipe(minifyCss())
             .pipe(gulp.dest(DIST_PATH))
